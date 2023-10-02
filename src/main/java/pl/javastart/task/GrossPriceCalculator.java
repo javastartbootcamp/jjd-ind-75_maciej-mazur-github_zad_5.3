0f169dd3a7a57c7af8d1cbc45a15ef097ed32a1e
Maciej Mazur <maciej.mazur.mailbox@gmail.com>
2023-10-02 21:25:02 +0200

    Naniosłem zalecone poprawki

--- src/main/java/pl/javastart/task/GrossPriceCalculator.java
+++ src/main/java/pl/javastart/task/GrossPriceCalculator.java
@@ -1,15 +1,21 @@
 package pl.javastart.task;
 
 class GrossPriceCalculator {
+    private static final double VAT_1 = 10.0;
+    private static final double VAT_2 = 20.0;
+    private static final double VAT_3 = 30.0;
+    private static final double VAT_4 = 40.0;
+    private static final double VAT_STANDARD = 25.0;
+
     double calculateGrossPrice(Product product) {
         String category = product.getCategory();
 
         return switch (category) {
-            case "warzywa" -> 1.1 * product.getNetPrice();
-            case "nabiał" -> 1.2 * product.getNetPrice();
-            case "meble" -> 1.3 * product.getNetPrice();
-            case "elektronika" -> 1.4 * product.getNetPrice();
-            default -> 1.2 * product.getNetPrice();
+            case Category.VEGETABLES -> (1 + VAT_1 / 100) * product.getNetPrice();
+            case Category.DAIRY -> (1 + VAT_2 / 100) * product.getNetPrice();
+            case Category.FURNITURE -> (1 + VAT_3 / 100) * product.getNetPrice();
+            case Category.ELECTRONICS -> (1 + VAT_4 / 100) * product.getNetPrice();
+            default -> (1 + VAT_STANDARD / 100) * product.getNetPrice();
         };
     }
 }
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,11 +4,11 @@ public class Main {
     private static GrossPriceCalculator grossPriceCalculator = new GrossPriceCalculator();
 
     public static void main(String[] args) {
-        Product product1 = new Product("Papryka", "Świeża papryka czerwona", 2.30, "warzywa");
-        Product product2 = new Product("Mleko", "Mleko UHT 2.0%", 3.50, "nabiał");
-        Product product3 = new Product("Komoda", "Komoda dębowa z szufladami", 435.0, "meble");
-        Product product4 = new Product("Smartphone", "Huawei p20 pro", 2600.0, "elektronika");
-        Product product5 = new Product("Hamburger", "Hamburger z podwójnym serem", 19.50, "fast food");
+        Product product1 = new Product("Papryka", "Świeża papryka czerwona", 2.30, Category.VEGETABLES);
+        Product product2 = new Product("Mleko", "Mleko UHT 2.0%", 3.50, Category.DAIRY);
+        Product product3 = new Product("Komoda", "Komoda dębowa z szufladami", 435.0, Category.FURNITURE);
+        Product product4 = new Product("Smartphone", "Huawei p20 pro", 2600.0, Category.ELECTRONICS);
+        Product product5 = new Product("Hamburger", "Hamburger z podwójnym serem", 19.50, Category.FAST_FOOD);
 
         printProductInfoWithGrossPrice(product1);
         printProductInfoWithGrossPrice(product2);
